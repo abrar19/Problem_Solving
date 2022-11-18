7510b2eff3eebb36a14f9002a9dd43c22d976203
Abrar Haque <abm.abrar@gmail.com>
2022-11-18 21:45:22 +0100

    With comments for better readability of Code

--- PlusOne.js
+++ PlusOne.js
@@ -3,14 +3,15 @@
  * @return {number[]}
  */
  var plusOne = function(digits) {
-    for(let i = digits.length - 1; i>= 0; i--){
-        if(digits[i] < 9){
-            digits[i]++
-            return digits
+    
+    for(let i = digits.length-1; i>=0; i--){ //reverse iteration of the given array
+        if(digits[i] < 9){ //need to increase by 1 if less than 9
+            digits[i] += 1;
+            return digits; //As we need to add 1 only so we will return the array asap we succeed 
+        }else{
+        digits[i] = 0; //if greater than 9, we will make this value 0 and try to increase 1 in the next index(reverse)
         }
-        
-        digits[i] = 0
     }
-    digits.unshift(1)
-    return digits
-}
+    digits.unshift(1);
+    return digits;
+};
