e03f63be6b1e7bae7f7ed797256fff879a30c7f1
Abrar Haque <abm.abrar@gmail.com>
2022-11-19 03:58:02 +0100

    Partial Solution of Leetcode problem - 67

---
+++ AddBinary.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var a = 1, b = 11;
+ var addBinary = function(a, b) {
+    var sum = parseInt(a) + parseInt(b);
+    console.log(sum)
+    return sum.toString(2);
+};
